bf67b9ddceb88ce1254afab4dbe60503a03b45bf
Patryk <patrykszczemczak@wp.pl>
2024-01-10 21:18:29 +0100

    vif

--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -1,45 +1,24 @@
 <template>
   <div>
-    <p>
-     You have <strong>{{ shares }}</strong> shares and their value is
-     <strong>{{ sharesValue }}$</strong>, because share price is
-     <strong>{{ sharePrice }}$</strong>
-    </p>
-     <button @click="changeNumberOfShares(1)">Buy one share</button>
-     <button @click="changeNumberOfShares(5)">Buy five shares</button>
-     <button @click="changeNumberOfShares(-5)">Sell five shares</button>
-     <button @click="changeNumberOfShares(-1)">Sell one share</button>
-    
+    <p>Masks: {{ masks }}</p>
+    <p v-if="masks>0">You can buy a mask!</p>
+    <p v-if="masks<=0">You can not buy a mask, it's out of stock...</p>
+    <button v-if="masks>0" @click="buyMask">Buy a mask</button>
   </div>
 </template>
 
 <script>
-import {ref, computed, watch} from 'vue'
+import {ref} from 'vue'
 
 export default {
   name: 'MyAwesomeComponent',
   setup(){
-
-    const shares = ref(15);
-    const sharePrice = ref(20);
-    const sharesValue = computed(()=>{return shares.value*sharePrice.value});
-
-    function changeNumberOfShares(number){
-      if(shares.value + number>=0){
-      shares.value+=number;}
+    const masks =ref(5);
+    
+    function buyMask(){
+      masks.value--;
     }
-
-    watch(shares, (shares, prevshares)=>{
-      shares > prevshares ? getPrice(1,5) : getPrice(-5,-1)
-    })
-
-    function getPrice(min, max){
-      const priceDiff = Math.floor(Math.random()* (max-min)+min)
-      if(sharePrice.value+priceDiff>=0){
-      sharePrice.value+=priceDiff;}
-    }
-
-    return{shares, sharePrice, sharesValue,changeNumberOfShares};
+    return {masks, buyMask}
   }
 };
 </script>
