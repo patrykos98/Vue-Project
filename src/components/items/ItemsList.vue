ca46a43215c1b554bd095b66a978747e499b12b6
Patryk <patrykszczemczak@wp.pl>
2024-01-11 19:33:09 +0100

    komponenty propsy

---
+++ src/components/items/ItemsList.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="items-list">
+    <SingleItem
+    v-for="(item, index) in items"
+      :key="item.id"
+      :index="index"
+      :item="item"
+      :first-name="item.firstName"
+      :last-name="item.lastName"
+      :age="item.age"
+    />
+  </div>
+</template>
+
+<script>
+import SingleItem from './item/SingleItem.vue'
+
+export default{
+    name:'ItemsList',
+    components:{SingleItem},
+    props:{
+        items:{
+            type:Array,
+            default:()=>([])
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.items-list {
+  font-family: arial;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 25px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/items/item/SingleItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <div
+      class="single-item"
+    >
+      <p>Candidate {{ index + 1 }}</p>
+      <p>Name: {{ firstName }} {{ lastName }}</p>
+      <p>Age: {{ age }}</p>
+      <p v-if="item.party">Party: {{ item.party }}</p>
+      <div v-if="item.opinionPool">
+        Opinion pool:
+        <progress max="100" :value="item.opinionPool"></progress>
+        {{ item.opinionPool }}%
+      </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'SingleItem',
+    props:{
+        item:{
+            type: Object,
+            default:()=>({})
+        },
+        index:{
+            type:Number,
+            default:0
+        },
+        firstName:{
+            type:String,
+            default:''
+        },
+        lastName:{
+            type:String,
+            default:''
+        },
+        age:{
+            type:Number,
+            default:0
+        }
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.single-item {
+    border: 1px solid black;
+    padding: 10px;
+  }
+
+</style>
