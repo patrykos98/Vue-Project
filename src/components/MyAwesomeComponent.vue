72c9331278bfa3f42b5827942faaaba0d06cdafa
Patryk <patrykszczemczak@wp.pl>
2024-01-11 20:12:25 +0100

    lifecycle hooks

--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -1,45 +1,36 @@
 <template>
-  <div>
-    <p>
-     You have <strong>{{ shares }}</strong> shares and their value is
-     <strong>{{ sharesValue }}$</strong>, because share price is
-     <strong>{{ sharePrice }}$</strong>
-    </p>
-     <button @click="changeNumberOfShares(1)">Buy one share</button>
-     <button @click="changeNumberOfShares(5)">Buy five shares</button>
-     <button @click="changeNumberOfShares(-5)">Sell five shares</button>
-     <button @click="changeNumberOfShares(-1)">Sell one share</button>
-    
-  </div>
+  <div></div>
 </template>
 
 <script>
-import {ref, computed, watch} from 'vue'
+import {onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted} from 'vue'
 
 export default {
   name: 'MyAwesomeComponent',
   setup(){
 
-    const shares = ref(15);
-    const sharePrice = ref(20);
-    const sharesValue = computed(()=>{return shares.value*sharePrice.value});
+    console.log('setup');
 
-    function changeNumberOfShares(number){
-      if(shares.value + number>=0){
-      shares.value+=number;}
-    }
+    onBeforeMount(()=>{
+      console.log('onBeforeMount');
+    });
+    onMounted(()=>{
+      console.log('onMounted');
+    });
+    onBeforeUpdate(()=>{
+      console.log('onBeforeUpdate');
+    });
+    onUpdated(()=>{
+      console.log('onUpdated');
+    });
+    onBeforeUnmount(()=>{
+      console.log('onBeforeUnmount');
+    });
+    onUnmounted(()=>{
+      console.log('onUnmounted');
+    });
 
-    watch(shares, (shares, prevshares)=>{
-      shares > prevshares ? getPrice(1,5) : getPrice(-5,-1)
-    })
-
-    function getPrice(min, max){
-      const priceDiff = Math.floor(Math.random()* (max-min)+min)
-      if(sharePrice.value+priceDiff>=0){
-      sharePrice.value+=priceDiff;}
-    }
-
-    return{shares, sharePrice, sharesValue,changeNumberOfShares};
+    return{};
   }
 };
 </script>
