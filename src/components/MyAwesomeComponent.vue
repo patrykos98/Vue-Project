a2bd7584239402da4a5356d47fa559e65b33df04
Patryk <patrykszczemczak@wp.pl>
2024-01-12 10:53:11 +0100

    suspense

--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -1,50 +1,40 @@
 <template>
-  <div
-      class="single-item"
-    >
-      <p>Candidate {{ index + 1 }}</p>
-      <p>Name: {{ firstName }} {{ lastName }}</p>
-      <p>Age: {{ age }}</p>
-      <p v-if="item.party">Party: {{ item.party }}</p>
-      <div v-if="item.opinionPool">
-        Opinion pool:
-        <progress max="100" :value="item.opinionPool"></progress>
-        {{ item.opinionPool }}%
-      </div>
-    </div>
+  <div>
+    <div v-if="error">Something went wrong.</div>
+    <Suspense>
+    <template #default>
+      <StarshipList/>
+    </template>
+    <template #fallback>
+      <AppLoader/>
+    </template>
+    
+  </Suspense>
+  </div>
 </template>
 
 <script>
-import {onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted} from 'vue'
-
+import{onErrorCaptured, ref} from 'vue'
+import StarshipList from './StarshipList.vue'
+import AppLoader from './AppLoader.vue';
 export default {
   name: 'MyAwesomeComponent',
+  components:{
+    StarshipList,
+    AppLoader
+  },
   setup(){
+    const error=ref(0)
 
-    console.log('setup');
-
-    onBeforeMount(()=>{
-      console.log('onBeforeMount');
-    });
-    onMounted(()=>{
-      console.log('onMounted');
-    });
-    onBeforeUpdate(()=>{
-      console.log('onBeforeUpdate');
-    });
-    onUpdated(()=>{
-      console.log('onUpdated');
-    });
-    onBeforeUnmount(()=>{
-      console.log('onBeforeUnmount');
-    });
-    onUnmounted(()=>{
-      console.log('onUnmounted');
-    });
-
-    return{};
+    onErrorCaptured(e=>{
+      error.value=e;
+      return true;
+    })
+    
+    return{error,onErrorCaptured}
   }
-};
+  }
+
 </script>
 
 
---
+++ src/components/StarshipList.vue
@@ -0,0 +1,34 @@
+<template>
+<section>
+    <h1>Ships list:</h1>
+    <StarshipItem v-for="(ship,index) in ships" :key="index" :ship="ship"/>
+</section>
+</template>
+
+<script>
+import StarshipItem from './StarshipItem.vue';
+import {ref} from 'vue'
+export default{
+    name:'StarshipList',
+    components:{
+        StarshipItem
+    },
+    async setup(){
+        const ships = ref(null)
+
+        const response= await fetch('https://swapi.dev/api/starships/')
+        const parsedResponse=await response.json()
+
+        ships.value=[...parsedResponse.results]
+
+        return{ships}
+    }
+
+    
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
