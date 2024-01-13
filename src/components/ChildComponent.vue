dfbd16559c523be65f3e4e8f0e6e376cb7646370
Patryk <patrykszczemczak@wp.pl>
2024-01-13 16:37:41 +0100

    testy propsow

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{ textprops }}  
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: "ChildComponent",
+    props:{
+        textprops: String
+    },
+    setup() {
+      
+      }
+    }
+  
+  </script>
+  
+  
+  
--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -1,66 +1,24 @@
 <template>
-  <div class="container">
-    <button class="button"
-      @click="handleMouseEvent"
-      @dblclick="handleMouseEvent"
-      @mousedown.right="handleMouseEvent"
-      @mouseup.middle="handleMouseEvent"
-      @mousemove.once="handleMouseEvent"
-      @mouseover="handleMouseEvent"
-      @mouseleave="handleMouseEvent"
-      @mousewheel="handleMouseEvent"
-      @mouseout="handleMouseEvent"
-      @contextmenu.prevent="handleMouseEvent"
-    >
-      Button
-    </button>
-    <form class="form" @submit.prevent="sendMessage">
-      <input 
-        type="text" 
-        class="input" 
-        placeholder="Type something"
-        @keypress.space="handleKeyEvent"
-        @keydown.shift.s.exact="handleKeyEvent"
-        @keyup.alt="handleKeyEvent"
-      />
-      <button class="button">Submit</button>
-    </form>
-    <p v-show="isMessageSend">Message send</p>
-    <div class="outer" @click="handleMouseEvent">
-      <div class="inner" @click.stop="handleMouseEvent"></div>
-    </div>
-    <ul>
-      <li v-for="(event, index) in mouseEventsArray" :key="index">{{event}}</li>
-    </ul>
-    <ul>
-      <li v-for="({key, type}, index) in keyEventsArray" :key="index">{{key}} {{type}}</li>
-    </ul>
+  <div>
+    <ChildComponent :textprops="text"/>  
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
 
 export default {
-  name: "MyAwsomeComponent",
-  setup() {
-    const mouseEventsArray = ref([])
-    const keyEventsArray = ref([])
-    const isMessageSend = ref(false)
+    name: "MyAwesomeComponent",
+    setup() {
+        const text = ref('Testujemy propsa :D');
+        const liczba = ref(1);
+        const isShow = true;
+        return { text, liczba, isShow };
+    },
+    components: { ChildComponent }
+}
 
-    function handleMouseEvent({type}) {
-      mouseEventsArray.value = [...mouseEventsArray.value, type]
-    }
-    function handleKeyEvent({key, type}) {
-      keyEventsArray.value = [...keyEventsArray.value, {key, type}]
-    }
-    function sendMessage() {
-      isMessageSend.value = true;
-    }
-
-    return {mouseEventsArray, handleMouseEvent, keyEventsArray, handleKeyEvent, sendMessage, isMessageSend}
-  }
-};
 </script>
 
 
