e464c2266e64277691d429e3aa32c0ba0d0dd7c9
Patryk <patrykszczemczak@wp.pl>
2024-01-29 22:17:04 +0100

    poczatki todo vue

--- src/components/MyAwesomeComponent.vue
+++ src/components/MyAwesomeComponent.vue
@@ -1,75 +1,60 @@
 <template>
-  <div class="container">
-    <BaseInput
-      name="input"
-      v-model:firstName="firstName"
-      v-model:lastName="lastName"
-    />
-    <p class="result">{{ firstName }}</p>
-    <p class="result">{{ lastName }}</p>
-    <button class="button" @click="revertText">Revert text</button>
-  </div>
+   <div class="to-do-container">
+    <div class="header">
+      <h1>ToDo List</h1>
+    </div>
+    <div class="to-do-elements">
+       <input type="text" placeholder="What you need to do?">
+    </div>
+    
+   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import BaseInput from './BaseInput';
+//import { ref } from 'vue';
 
 export default {
   name: 'MyAwsomeComponent',
   components: {
-    BaseInput,
+    
   },
   setup() {
-    const text = ref('');
-    const firstName = ref('');
-    const lastName = ref('');
+    
 
-    function revertText() {
-      firstName.value = firstName.value
-        .split('')
-        .reverse()
-        .join('');
-      lastName.value = lastName.value
-        .split('')
-        .reverse()
-        .join('');
-    }
-
-    return { text, firstName, lastName, revertText };
+    return { };
   },
 };
 </script>
 
 <style lang="scss">
-.container {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-}
-.button {
-  cursor: pointer;
-  background-color: transparent;
-  color: #000;
-  border: 3px solid #000;
-  padding: 10px 20px;
-  font-size: 24px;
-  outline: none;
+body{
   display: flex;
   justify-content: center;
   align-items: center;
-  transition: background-color 0.1s ease-in, color 0.1s ease-in,
-    border-color 0.1s ease-in;
-  margin-top: 10px;
-  &:hover {
-    background-color: #000;
-    color: #fff;
-    border-color: #000;
-  }
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  height: 100vh;
+  width: 100vw;
+  margin:0;
+  padding:0;
+  background-color: #99CCFF;
+  background: linear-gradient(to right top, #99CCFF, #0066CC);
 }
-.result {
-  margin-top: 100px;
+
+.to-do-container{
+  display:flex;
+  flex-direction: column;
+  min-height: 45vh;
+  min-width: 30vw;
+  background-color: azure;
+  box-shadow: 0px 0px 35px black;
+  border-radius: 5px;
+}
+
+.header{
+  align-self: center;
+}
+
+.to-do-elements{
+  align-self: center;
 }
 </style>
